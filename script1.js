3663178539791b7f7bbf6a5f7565592f4922fffb
khsdbfibibdf <123285533+khsdbfibibdf@users.noreply.github.com>
2024-11-02 00:31:32 -0700

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+// script1.js
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting GoldenStudios! We will get back to you soon.');
+  });
+  
